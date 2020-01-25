f32bade170f62915b9d90bbd5612ef6ed5187843
E4U~Allen <49747518+allenenosh@users.noreply.github.com>
2020-01-25 15:13:42 +0530

    Update scan.py

--- scan.py
+++ scan.py
@@ -34,7 +34,7 @@ def main():
     print("  |CS   '--'     | ")
     print("{}  '--------------'----------{}------------------.  ").format(r,w)
     print("{}  | {}Author  : {}E4U {}     | {}IND{}I{}{}A        | ").format(r,w,r,w,r,ir,reset,w)
-    print("{}  | {}Youtube : {}null {}| {}null{}|").format(r,w,w,w,lgray,w)
+    print("{}  | {}cracked : {}null {}| {}null{}|").format(r,w,w,w,lgray,w)
     print("{}  '------------------------------------{}-------'  ").format(r,w)
     print ("  {}[ 1 ] {}Italy").format(r,w)
     print ("  {}[ 2 ] {}Indonesia").format(r,w)
